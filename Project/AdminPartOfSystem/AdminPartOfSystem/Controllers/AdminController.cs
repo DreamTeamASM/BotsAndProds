0d71524cd06290bf9bc8b6103bb8cf7e925c6dfc
artem-shapranov2703 <43373198+artem-shapranov2703@users.noreply.github.com>
2018-10-12 11:23:28 +0600

    AdminPanelVer1

---
+++ Project/AdminPartOfSystem/AdminPartOfSystem/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using AdminPartOfSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminPartOfSystem.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        BotsAndProdsEntities1 context = new BotsAndProdsEntities1();
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Products prod)
+        {
+            try
+            {
+                context.Insert_Product(prod.Name, prod.Price);
+                return RedirectToAction("Products");
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = @"Товар не добавлен т.к. 
+он уже содержится в БД";
+            }
+            return View();   
+        }
+
+        public ActionResult Products()
+        {
+
+            return View(context.Products.ToList());
+        }
+    }
+}
---
+++ Project/AdminPartOfSystem/AdminPartOfSystem/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdminPartOfSystem.Models;
+namespace AdminPartOfSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string login,string pass)
+        {
+            BotsAndProdsEntities1 context = new BotsAndProdsEntities1();
+          
+            List<Authorize_Result> per= context.Authorize(login, pass).ToList();
+            if (per.Count>0)
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
